366296e4d4c952bf221c1829ccca32b7956aa782
efletch <ecfletch@gmail.com>
2013-11-30 21:05:04 -0800

    mysql queries and inserts for survey results

--- data/survey_insert.php
+++ data/survey_insert.php
@@ -4,6 +4,8 @@ include("../lib/inc.php");
 /* Store information from survey in MySQL database
  * ====================================================================== */
 
+$mysql = mysql();
+
 $car_table = array(
     "car_year" => $_POST['caryear'],
     "car_make" => $_POST['carmake'],
@@ -15,25 +17,58 @@ $car_table = array(
     "car_co2" => $_POST['carco2'],
 );
 
-//insert into mysql table
-//return unique_id as carid
+//insert vehicle into mysql table
+//return carid
+$q = "
+INSERT INTO vehicle (vehicle_id,  year, 
+                     make, model, fuel_type,
+                     mpg_city, mpg_hwy, co2)
+  VALUES($car_table['car_vid'], 
+         $car_table['car_year'],
+	 $car_table['car_make'],
+	 $car_table['car_model'],
+	 $car_table['car_fuel_type'],
+	 $car_table['car_mpg_city'],
+	 $car_table['car_mpg_hwy'],
+	 $car_table['car_co2'])
+";
+
+$carid = $mysql->insert($q);
 
 $trip_table = array(
    "trip_distance" => $_POST['tripdistance'],
-   "trip_road_type" => $_POST['roadtype'],
+   "trip_hwy_per" => $_POST['hwyper'],
 );
 
-//insert into mysql table
-//return unique_id as tripid
+//insert trip in mysql table
+//return tripid
+$q = "
+INSERT INTO trip (distance, hwy_per) 
+  VALUES($trip_table[trip_distance],
+	 $trip_table[trip_hwy_per])
+";
+
+$tripid = $myql->insert($q);
 
 $survey_table = array(
-   "survey_carid" => $carid,
-   "survey_tripid" => $tripid,
    "survey_freq" => $_POST['freq'],
    "survey_zipcode" => $_POST['zip'],
-   "survey_vehicle_nospec" => $_POST['vehicle_nonspec'],
-   "survey_created_at" => date('Y-m-d h:i:s A'),
+   "survey_vehicle_nonspec" => $_POST['vehicle_nonspec'],
+   "survey_created_at" => date('Y-m-d h:i:s A')
 );
+
+$q = "
+INSERT INTO survey (vehicle_id, trip_id, freq,
+       	    	    zipcode, vehicle_nonspec, created_at)
+   VALUES($carid, $tripid,
+          $survey_table['survey_freq'],
+	  $survey_table['survey_zipcode'],
+	  $survey_table['survey_vehicle_nonspec'],
+	  now())
+
+";
+
+$mysql->insert($q);
    
 
 ?>
