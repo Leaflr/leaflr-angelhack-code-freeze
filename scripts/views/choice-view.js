0a59a2857aacc545bb6e2657278ef436db0031c0
Andy Axton <dizzysfarm@gmail.com>
2013-11-30 22:40:42 -0800

    animations n shit

--- scripts/views/choice-view.js
+++ scripts/views/choice-view.js
@@ -13,8 +13,21 @@ function( Backbone, Communicator, choiceTemp ){
   			'click':'selected'
   		},
 
+      onRender: function(){
+        this.$el.fadeIn(1200);
+      },
+
   		selected: function(){
-  			Communicator.events.trigger('nextStep', this.model.get('nextStep') );
+        var self = this;
+        
+        this.$el.find('.icon').addClass('checked');
+        this.$el.find('.icon-svg').addClass('checked').attr('src','img/icons/checkmark.svg');
+        this.$el.siblings().find('.icon').addClass('not-checked');
+        this.$el.addClass('fade-out').siblings().addClass('fade-out');
+    		
+        window.setTimeout(function(){
+          Communicator.events.trigger('nextStep', self.model.get('nextStep') );
+        }, 1000); 
 
   			if (this.model.get('onSelect'))
   			this.model.attributes.onSelect();
