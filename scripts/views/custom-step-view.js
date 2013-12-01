9e25983a4c71fa18d500f03f0eb0ccb8201e350c
Kaleb Clark <kaleb@abraxxus.net>
2013-12-01 12:17:17 -0800

    Data correct

--- scripts/views/custom-step-view.js
+++ scripts/views/custom-step-view.js
@@ -109,15 +109,16 @@ define(['backbone','communicator'], function( Backbone, Communicator ){
             data = eval('('+data+')');
 
             console.log('FULL', data);
-            self.model.collection.parents[0].results.car_year = year;
-            self.model.collection.parents[0].results.car_make = make;
-            self.model.collection.parents[0].results.car_model = model;
+            self.model.collection.parents[0].results.caryear = year;
+            self.model.collection.parents[0].results.carmake = make;
+            self.model.collection.parents[0].results.carmodel = model;
+            self.model.collection.parents[0].results.vehicle_nonspec = 1;
             if(option != null) {
-              self.model.collection.parents[0].results.car_id = option;
+              self.model.collection.parents[0].results.carvid = option;
             }
-            self.model.collection.parents[0].results.mph_hwy  = data.highway08;
-            self.model.collection.parents[0].results.mph_city = data.city08;
-            self.model.collection.parents[0].results.co2      = data.co2TailpipeGpm;
+            self.model.collection.parents[0].results.carmpghwy  = data.highway08;
+            self.model.collection.parents[0].results.carmpgcity = data.city08;
+            self.model.collection.parents[0].results.carco2      = data.co2TailpipeGpm;
 
 
             self.triggerNext();
